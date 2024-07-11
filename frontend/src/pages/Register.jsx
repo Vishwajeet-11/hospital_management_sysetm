c56c5903ebdde7f3e29a04a8625671829b03f60a
Vishwajeet-11 <vishwajeetbharadiya12@gmail.com>
2024-07-12 00:40:16 +0530

    register page completed, toastify sometimes doesn't work efficiently (framework side bug), abouUs, appointment left

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+import React, { useContext, useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { Context } from "../main";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const { isAuthenticated, setIsAuthenticated } = useContext(Context);
 
-export default Register
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+  const [aadhaar_number, setAadhaar_number] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigateTo = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:4000/api/v1/user/patient/register",
+        {firstName, lastName, email, phone, aadhaar_number, dob, gender, password, role:"Patient"},
+        {
+          withCredentials: true,
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      toast.success(response.data.message);
+      // setIsAuthenticated(true);
+      navigateTo("/Login");
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to={"/"} />;
+  }
+
+  return (
+    <div className="container form-component register-form">
+      <h2>Sign Up</h2>
+      <p>please signUp to continue</p>
+      <p>
+        Enim anim exercitation mollit eiusmod consequat est officia anim quis
+        exercitation officia irure proident incididunt.
+      </p>
+      <form onSubmit={handleRegister}>
+        <div>
+          <input
+            type="text"
+            placeholder="First Name"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="Phone"
+            value={phone}
+            onChange={(e) => setPhone(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <input
+            type="number"
+            placeholder="Aadhaar Number"
+            value={aadhaar_number}
+            onChange={(e) => setAadhaar_number(e.target.value)}
+          />
+          <input
+            type="date"
+            placeholder="Date Of Birth"
+            value={dob}
+            onChange={(e) => setDob(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <select value={gender} onChange={(e) => setGender(e.target.value)}>
+            <option value="">Select Gender</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
+          </select>
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div
+          style={{
+            gap: "10px",
+            justifyContent: "flex-end",
+            flexDirection: "row",
+          }}
+        >
+          <p style={{ marginBottom: 0 }}>Already Registered ? </p>
+          <Link
+            to={"/Login"}
+            style={{ textDecoration: "none", alignItems: "center" }}
+          >
+            Login Now
+          </Link>
+        </div>
+        <div style={{ justifyContent: "center", alignItems: "center" }}>
+          <button type="submit" style={{cursor: "pointer"}}>Register</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
